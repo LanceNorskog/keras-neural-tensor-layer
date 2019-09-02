4ffedd517a56ee71ae6bd237648bf55a52d2b469
Lance Norskog <lance.norskog@walmartlabs.com>
2019-09-02 16:37:39 -0700

    done

--- ntn_diag.py
+++ ntn_diag.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-import scipy.stats as stats
-
 from keras import backend as K
 from keras.engine.topology import Layer
+import scipy.stats as stats
 
 class NeuralTensorDiagLayer(Layer):
   def __init__(self, output_dim, input_dim=None, **kwargs):
@@ -24,7 +23,7 @@ class NeuralTensorDiagLayer(Layer):
     initial_V_values = stats.truncnorm.rvs(-2 * std, 2 * std, loc=mean, scale=std, size=(2*d,k))
     self.W = K.variable(initial_W_values, name='W')
     self.V = K.variable(initial_V_values, name='V')
-    self.b = K.zeros((self.input_dim,), name='b')
+    self.b = K.zeros((self.input_dim * 2,), name='b')
     self.trainable_weights = [self.W, self.V, self.b]
 
 
@@ -34,17 +33,24 @@ class NeuralTensorDiagLayer(Layer):
                       '(at least 2). Got: ' + str(inputs))
     e1 = inputs[0]
     e2 = inputs[1]
-    batch_size = K.shape(e1)[0]
+    #batch_size = K.shape(e1)[0]
+    #print('batch_size: ', batch_size)
     k = self.output_dim
-    print('inputs: ', [e1,e2])
-    feed_forward_product = K.dot(K.concatenate([e1,e2]), self.V)
-    print('ff: ', feed_forward_product)
+    #print('inputs: ', [e1,e2])
+    feed_forward_product = K.dot(K.concatenate([e1,e2]) + self.b, self.V)
+    #print('ff: ', feed_forward_product)
+    #print('d1: ', e1 * self.W[0])
+    #print('d2: ', e2 * (e1 * self.W[0]))
+    #print('d3: ', e2 * (e1 * self.W[0]) + self.b)
     diag_tensor_products = [] 
     for i in range(k):
-      diag_tensor_products.append((e2 * K.dot(e1, self.W[i])) + self.b, axis=1)
-    print('diag.shape: ', K.shape(diag_tensor_products[0]))
-    result = K.tanh(K.reshape(K.concatenate(diag_tensor_products, axis=0), (batch_size, k)) + feed_forward_product)
-    print('result: ', result)
+      diag_tensor_products.append(K.sum(e2 * (e1 * self.W[i])))
+    #print('diag.shape: ', K.shape(diag_tensor_products[0]))
+    #print('o1: ', K.stack(diag_tensor_products))
+    #print('o2: ', K.reshape(K.concatenate(diag_tensor_products, axis=1), (batch_size, k)))
+    #print('o3: ', K.reshape(K.concatenate(diag_tensor_products, axis=1), (-1, k)) + feed_forward_product)
+    result = K.tanh(K.stack(diag_tensor_products) + feed_forward_product)
+    #print('result: ', result)
     return result
 
 
